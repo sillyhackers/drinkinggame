de203e3c38e328f115dfa60abaafde4091b97846
Paul Amazona <netfiddler@yahoo.com>
2018-12-15 12:39:41 +0800

    initial version

---
+++ ClientApp/app/components/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+}
